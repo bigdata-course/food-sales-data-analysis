1c0b91da00ddb5be58abc4b993e08d0715cf7369
chenwhql <sunny_cwh@163.com>
2017-01-04 10:20:54 +0800

    数据分析代码提交

---
+++ readme.sh
@@ -0,0 +1,10 @@
+sudo pip install numpy
+sudo sudo apt-get install python-matplotlib ipython ipython-notebook
+sudo apt-get install python-pandas python-sympy python-nose
+sudo pip install scipy
+sudo pip install scikit-learn
+
+python proc.py
+#聚类和回归算法调用了sklearn.
+#spark版本使用了mllib
+
